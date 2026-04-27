5cf0d36c6950afc09700e99450d880e271db84e6
raihanrosadi07 <raihanrosadi284@gmail.com>
2026-04-27 18:11:59 +0700

    void int main

--- friendFunctionSatu.cpp
+++ friendFunctionSatu.cpp
@@ -8,3 +8,16 @@ private:
 public:
     friend void setNama(mahasiswa &a, string);
 };
+
+void setNama(mahasiswa &a, string b)
+{
+    a.nama = b;
+    cout << a.nama;
+}
+
+int main()
+{
+    mahasiswa joko;
+    setNama(joko, "kairi Kumar");
+    return 0;
+}
